6419ee5dda65742a5ffea82b04f310b4ea4a5cec
Vaibhav varunkar <vaibhavvarunkar2001@gmail.com>
2021-03-23 21:33:20 +0530

    dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+const dotenv = require('dotenv');
 
 require('dotenv').config();
 

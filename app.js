5065830f1de5ef8875a002008747255a75b0b8cc
Vaibhav varunkar <vaibhavvarunkar2001@gmail.com>
2021-03-23 21:38:36 +0530

    PORT

--- app.js
+++ app.js
@@ -20,13 +20,18 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = process.env.DB_CONN;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000 || process.env.PORT))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true });
+  
 
 // routes
 app.get("*", checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}...`);
+})
 
